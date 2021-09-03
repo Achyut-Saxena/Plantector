9f7e024090bab9e6487be71b3ca596da2cc6b52d
Achyut-Saxena <69965983+Achyut-Saxena@users.noreply.github.com>
2021-09-04 01:05:35 +0530

    5V Passive Buzzer Test

---
+++ Codes/Test Codes/buzzer_test.ino
@@ -0,0 +1,13 @@
+void setup() 
+{}
+void loop() 
+{
+    tone(8, 1400, 500);
+    delay(200);
+    tone(8, 800, 200);
+    delay(200);
+    tone(8, 1800, 500);
+    delay(200);
+    tone(8, 600, 200);
+    delay(200);
+}
